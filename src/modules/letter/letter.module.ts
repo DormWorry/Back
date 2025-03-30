3c80f5ec482477efd6eb39be994a94ca166ab1a8
kwon <dohun_dev@naver.com>
2025-03-30 13:26:26 +0900

    feat: User 엔티티 연동 추가
    - Letter 모듈에 User 엔티티 의존성 추가
    - 방 번호로 사용자 조회 기능 구현
    - 수신자 ID 자동 설정 기능 추가
    - 존재하지 않는 방 번호에 대한 예외 처리 추가

--- src/modules/letter/letter.module.ts
+++ src/modules/letter/letter.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Letter } from './entities/letter.entity';
 import { LetterService } from './letter.service';
 import { LetterController } from './letter.controller';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Letter])],
+  imports: [TypeOrmModule.forFeature([Letter, User])],
   controllers: [LetterController],
   providers: [LetterService],
   exports: [LetterService],
